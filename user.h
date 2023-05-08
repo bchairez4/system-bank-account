0f601706b5c4fdcf2af4d5b41101c1c7fa21f1ca
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-07 23:19:40 -0700

    created basic user class methods

--- user.h
+++ user.h
@@ -1,1 +1,24 @@
+#ifndef USER_H
+#define USER_H
 
+#include <string>
+
+class User {
+    private:
+        std::string name_;
+        std::string email_;
+        std::string password_;
+    public:
+        User();
+        User(const std::string& name, const std::string& email, const std::string& password);
+        User(const User& other);
+        ~User();
+        std::string getName() const;
+        std::string getEmail() const;
+        std::string getPassword() const;
+        void setName(const std::string fName, const std::string lName);
+        void setEmail(const std::string email);
+        void setPassword(const std::string password);
+};
+
+#endif
