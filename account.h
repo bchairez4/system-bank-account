e8f0dc6ac572fa5db41668ec9549a2320d613b91
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-08 23:41:06 -0700

    created basic account methods

--- account.h
+++ account.h
@@ -1,4 +1,28 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
+#include <string>
+
+class Account {
+    private:
+        std::string name_;
+        int balance_;
+        int accountNumber_;
+        int routingNumber_;
+    public:
+        Account();
+        Account(const std::string& name, const int& balance, const int& accountNumber, const int& routingNumber);
+        Account(const Account& other);
+        ~Account();
+        std::string getName() const;
+        int getBalance() const;
+        int getAccountNumber() const;
+        int getRoutingNumber() const;
+        void setName(const std::string& name);
+        void setBalance(const int& balance);
+        void setAccountNumber(const int& accountNumber);
+        void setRoutingNumber(const int& routingNumber);
+        void printBalance() const;
+};
+
 #endif
