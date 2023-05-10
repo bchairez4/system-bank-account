3c4711f1ce675878334faa645df4868a60a07c13
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-10 12:33:54 -0700

    added pin number as private data

--- client.h
+++ client.h
@@ -8,18 +8,21 @@
 class Client {
     private:
         User user_;
+        int pin_;
         std::vector<Account> accounts_;
     public:
         Client();
-        Client(const User& user, const std::vector<Account>& accounts);
+        Client(const User& user, const int& pin, const std::vector<Account>& accounts);
         Client(const Client& other);
         ~Client();
         Client& operator=(const Client& other);
         User getUser() const;
+        int getPin() const;
         std::vector<Account> getAccounts() const;
         void updateUserName(const std::string& fName, const std::string& lName);
         void updateUserEmail(const std::string& email);
         void updateUserPassword(const std::string& password);
+        void updatePin(const int& pin);
         void updateAccountName(const std::string& accountName, const std::string& updatedName);
         void addAccount(const Account& account);
         void removeAccount(const std::string& accountName);
