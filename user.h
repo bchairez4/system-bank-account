9d7c9be4f86696f6acaa5b190c2fe80d632faf39
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-10 18:56:18 -0700

    updated user class methods

--- user.h
+++ user.h
@@ -3,21 +3,25 @@
 
 #include <string>
 
+// Base Class
 class User {
     private:
-        std::string name_;
+        std::string firstName_;
+        std::string lastName_;
         std::string email_;
         std::string password_;
     public:
         User();
-        User(const std::string& name, const std::string& email, const std::string& password);
+        User(const std::string& firstName, const std::string& lastName, const std::string& email, const std::string& password);
         User(const User& other);
         ~User();
         User& operator=(const User& other);
-        std::string getName() const;
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getFullName() const;
         std::string getEmail() const;
         std::string getPassword() const;
-        void setName(const std::string fName, const std::string lName);
+        void setName(const std::string firstName, const std::string lastName);
         void setEmail(const std::string email);
         void setPassword(const std::string password);
 };
