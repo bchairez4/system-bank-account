30e7ae3d7da103edce6a042796f84e7b7e85eae1
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-14 12:57:05 -0700

    updated header file functions

--- database.h
+++ database.h
@@ -5,6 +5,9 @@
 #include <unordered_map>
 #include <fstream>
 
+// Key is a string comprised of a concatonation of Client's password and last name. (In theory, both should be unique enough to almost never cause collisions.) 
+// Value is the Client object containing all pertinent data.
+
 class Database {
     private:
         std::unordered_map<std::string, Client> db_;
@@ -14,10 +17,14 @@ class Database {
         Database(const Database& other);
         ~Database();
         Database& operator=(const Database& other);
+        std::unordered_map<std::string, Client>::iterator find(const Client& client);
+        std::unordered_map<std::string, Client> getDatabase() const;
+        void displayTable() const;
         void save();
         void load(const std::string file);
         void update();
-        void remove();
+        void add(const Client& client);
+        void remove(const Client& client);
 };
 
 #endif
