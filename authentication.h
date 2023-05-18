50215dde50b04408d85f1adbd209634883dbad79
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-17 18:57:58 -0700

    added methods to authentication source file

--- authentication.h
+++ authentication.h
@@ -1,6 +1,22 @@
 #ifndef AUTHENTICATION_H
 #define AUTHENTICATION_H
 
-class Authentication {};
+#include "database.cpp"
+
+class Authentication {
+    private:
+        Client currentUser_;
+        bool signedIn_;
+    public:
+        Authentication();
+        Authentication(const std::string& email, const std::string& password, const Database& database);
+        Authentication(const Authentication& other);
+        ~Authentication();
+        Authentication& operator=(const Authentication& other);
+        Client getCurrentUser() const;
+        bool isSignedIn() const;
+        void signIn(const std::string& email, const std::string& password, const Database& database);
+        void signOff();
+};
 
 #endif
