74afe72072d810e0201e727ed1c638b7d45a6c96
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-17 17:51:58 -0700

    updated key structure

--- database.cpp
+++ database.cpp
@@ -21,7 +21,7 @@ Database& Database::operator=(const Database& other) {
 }
 
 std::unordered_map<std::string, Client>::iterator Database::find(const Client& client) {
-    std::unordered_map<std::string, Client>::iterator it = db_.find((client.getPassword() + client.getLastName()));
+    std::unordered_map<std::string, Client>::iterator it = db_.find((client.getEmail() + client.getPassword()));
     return it;
 }
 
@@ -96,7 +96,7 @@ void Database::load(const std::string& fileName) {
             }
 
             Client tempClient(firstName, lastName, email, password, pin, accounts);
-            db_.insert({(password + lastName), tempClient});
+            db_.insert({(email + password), tempClient});
         }
     }
 
@@ -109,7 +109,7 @@ void Database::update(const Client& oldClient, const Client& newClient) {
 
     if (it != db_.end()) {
         db_.erase(it);
-        db_.insert({(newClient.getPassword() + newClient.getLastName()), newClient});
+        db_.insert({(newClient.getEmail() + newClient.getPassword()), newClient});
     }
 }
 
@@ -121,7 +121,7 @@ void Database::add(const Client& client) {
         return;     //already exists, exit out of function
     }
 
-    db_.insert({(client.getPassword() + client.getLastName()), client});
+    db_.insert({(client.getEmail() + client.getPassword()), client});
 }
 
 // Removes a Client from the database
