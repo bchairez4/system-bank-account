15b6a05aa2d9e6bcd2468643e88ef3d7258e19db
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-22 15:36:44 -0700

    added new methods

--- user.h
+++ user.h
@@ -21,6 +21,8 @@ class User {
         std::string getFullName() const;
         std::string getEmail() const;
         std::string getPassword() const;
+        void setFirstName(const std::string& firstName);
+        void setLastName(const std::string& lastName);
         void setName(const std::string firstName, const std::string lastName);
         void setEmail(const std::string email);
         void setPassword(const std::string password);
