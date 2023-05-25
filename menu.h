506dcbd26cda5eda02796b96a007d088005b00b3
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-24 19:31:53 -0700

    updated header file

--- menu.h
+++ menu.h
@@ -1,6 +1,32 @@
 #ifndef MENU_H
 #define MENU_H
 
-class Menu {};
+#include "system.cpp"
+
+class Menu {
+    private:
+        System sys_;
+    public:
+        Menu();
+        Menu(const Menu& other);
+        ~Menu();
+        Menu& operator=(const Menu& other);
+        void displayWelcome() const;
+        void displayFarewell() const;
+        void displayStartMenu() const;
+        void displayFullMenu() const;
+        void start();
+        void full();
+        void signIn();
+        void signUp();
+        void signOut();
+        void deposit();
+        void withdrawl();
+        void openAccount();
+        void closeAccount();
+        void updateAccount();
+        void editUserProfile();
+        void quit();
+};
 
 #endif
