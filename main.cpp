1bb6ced3525c66ecbfa21f911b7917c5559fc32a
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-26 21:24:15 -0700

    created main.cpp
    This file encapsulates the whole project so one instance of the Bank System can run.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*****************************************************************************
+*    Brian Chairez                                                           *
+*    Goal: Implementing a Bank Account System using a faux database.         *
+*****************************************************************************/
+
+#include "menu.cpp"
+
+int main(int argc, char* argv[]) {
+    Menu one;
+    return 0;
+}
