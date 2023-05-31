d0321daad464e1973b82d55f12c009f4744867f1
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-05-30 20:11:29 -0700

    added new method

--- system.h
+++ system.h
@@ -24,10 +24,11 @@ class System {
         void addCustomer(const Client& client);
         void removeCustomer(const Client& client);
         void updateCustomer(const Client& oldClient, const Client& newClient);
+        Account getCustomerAccount(const Client& client, const std::string& accountName) const;
+        int getCustomerAccountBalance(const Client& client, const std::string& accountName) const;
         void addAccount(const Client& client, const Account& account);
         void closeAccount(const Client& client, const std::string& accountName);
         void updateCustomerAccount(const Client& client, const Account& oldAccount, const Account& newAccount);
-        int getCustomerAccountBalance(const Client& client, const std::string& accountName) const;
         void deposit(const int& pin, const std::string& accountName, const int& amount);
         void withdrawl(const int& pin, const std::string& accountName, const int& amount);
 };
